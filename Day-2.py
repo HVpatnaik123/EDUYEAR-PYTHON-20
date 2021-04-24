08cee2e93c24683edba8622f7c945e017045c9d5
HVpatnaik123 <46641626+HVpatnaik123@users.noreply.github.com>
2021-04-24 23:55:23 +0530

    Day-2

---
+++ Day-2.py
@@ -0,0 +1,19 @@
+#Graduation data
+
+Trade = "Mechanical Engineering"
+CGPA = 7.02
+passout = 2014
+job_status = True
+
+print("my branch is: {}".format(Trade))
+print("my grade point is: {}".format(CGPA))
+print("Passout year is: {}".format(passout))
+print("am i working? {}".format(job_status))
+
+print("-------------------------------")
+Trade = "Mechanical Engineering"
+CGPA = 7.02
+passout = 2014
+job_status = True
+
+print("Branch: {}\nGradepoint: {}\nPassout year: {}\njob holder: {}".format(Trade,CGPA,passout,job_status))
